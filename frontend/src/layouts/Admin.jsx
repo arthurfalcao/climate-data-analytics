24eff8f2b67a4d4cd87d661679a251a7a158a8e5
Arthur Falcão <arthurfalcao77@gmail.com>
2020-05-24 17:39:19 +0100

    :art: change admin component to function

--- frontend/src/layouts/Admin.jsx
+++ frontend/src/layouts/Admin.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import t from 'prop-types';
 import { Container } from 'reactstrap';
+
 import AdminNavbar from 'components/Navbar/AdminNavbar';
 import AdminFooter from 'components/Footer/AdminFooter';
 import Sidebar from 'components/Sidebar';
 
-class Admin extends React.Component {
-  componentDidUpdate(e) {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-    this.refs.mainContent.scrollTop = 0;
-  }
+function Admin(props) {
+  const { children } = props;
 
   // getBrandText(path) {
   //   for (let i = 0; i < routes.length; i++) {
@@ -20,31 +18,33 @@ class Admin extends React.Component {
   //   return 'Brand';
   // }
 
-  render() {
-    return (
-      <>
-        <Sidebar
-          {...this.props}
-          // routes={routes}
-          logo={{
-            innerLink: '/admin/index',
-            // imgSrc: require('assets/img/brand/argon-react.png'),
-            imgAlt: '...',
-          }}
+  return (
+    <>
+      <Sidebar
+        {...props}
+        // routes={routes}
+        logo={{
+          innerLink: '/admin/index',
+          // imgSrc: require('assets/img/brand/argon-react.png'),
+          imgAlt: '...',
+        }}
+      />
+      <div className="main-content">
+        <AdminNavbar
+          {...props}
+          // brandText={this.getBrandText(this.props.location.pathname)}
         />
-        <div className="main-content" ref="mainContent">
-          <AdminNavbar
-            {...this.props}
-            // brandText={this.getBrandText(this.props.location.pathname)}
-          />
-          {this.props.children}
-          <Container fluid>
-            <AdminFooter />
-          </Container>
-        </div>
-      </>
-    );
-  }
+        {children}
+        <Container fluid>
+          <AdminFooter />
+        </Container>
+      </div>
+    </>
+  );
 }
+
+Admin.propTypes = {
+  children: t.node.isRequired,
+};
 
 export default Admin;
