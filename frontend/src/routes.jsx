b271a56deeae6a4285584378d2f57f0a2ea02f3c
Arthur Falcão <arthurfalcao77@gmail.com>
2020-05-30 17:34:31 +0100

    :art: add lazy loading

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Auth from 'layouts/Auth';
-import Admin from 'layouts/Admin';
-import Dashboard from 'pages/Dashboard';
-import Profile from 'pages/Profile';
-import Login from 'pages/Login';
-import Register from 'pages/Register';
-import Home from 'pages/Home';
+const Auth = lazy(() => import('layouts/Auth'));
+const Admin = lazy(() => import('layouts/Admin'));
+const Dashboard = lazy(() => import('pages/Dashboard'));
+const Profile = lazy(() => import('pages/Profile'));
+const Login = lazy(() => import('pages/Login'));
+const Register = lazy(() => import('pages/Register'));
+const Home = lazy(() => import('pages/Home'));
 
 function Routes() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    <Suspense fallback="Loading...">
+      <Switch>
+        <Route exact path="/" component={Home} />
 
-      <Route path="/weather">
-        <Admin>
-          <Dashboard />
-        </Admin>
-      </Route>
+        <Route path="/weather">
+          <Admin>
+            <Dashboard />
+          </Admin>
+        </Route>
 
-      <Route path="/profile">
-        <Admin>
-          <Profile />
-        </Admin>
-      </Route>
+        <Route path="/profile">
+          <Admin>
+            <Profile />
+          </Admin>
+        </Route>
 
-      <Route path="/login">
-        <Auth>
-          <Login />
-        </Auth>
-      </Route>
+        <Route path="/login">
+          <Auth>
+            <Login />
+          </Auth>
+        </Route>
 
-      <Route path="/register">
-        <Auth>
-          <Register />
-        </Auth>
-      </Route>
-    </Switch>
+        <Route path="/register">
+          <Auth>
+            <Register />
+          </Auth>
+        </Route>
+      </Switch>
+    </Suspense>
   );
 }
 
